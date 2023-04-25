8d5353b9cef8abbcc0071be65ff3fbcbe38fc98f
MD jABER HOSSEN <94259630+jaber-it@users.noreply.github.com>
2023-04-25 23:14:01 +0600

    Update and rename Script22.js to Script23.js

--- Sliding-Sign-In-Sign-Up-Form-master/BlogSite_Use_Fole/Script23.js
+++ Sliding-Sign-In-Sign-Up-Form-master/BlogSite_Use_Fole/Script23.js
@@ -10,8 +10,6 @@ const logoutDropdownMenu = document.getElementById('logout-dropdown-menu');
 const fullbodydiv = document.querySelector('.fullbody');
 const fullbodydiv2 = document.querySelector('.fullbody2');
 const jwt = sessionStorage.getItem("jwt");
-document.getElementById('avatar2').src = sessionStorage.getItem("pp_url");
-document.getElementById('avatar3').src = sessionStorage.getItem("pp_url");
 
 
 loginLink.addEventListener('click', function () {
@@ -432,13 +430,13 @@ function loadUser(jwt) {
       console.log(response);
       if (response.items.length > 0) {
         const user = response.items[0];
-        document.getElementById("avatar3").src = user.pp_url; 
-        document.getElementById("full_name").innerHTML = user.full_name;
-        document.getElementById("avatar2").src = user.pp_url;
-        document.getElementById("fname2").innerHTML = user.full_name;
-        document.getElementById("phone_number").innerHTML = user.phone_number;
-        document.getElementById("email_address").innerHTML = user.email_address;
-        document.getElementById("user_type").innerHTML = user.user_type;
+          document.getElementById("avatar3").src = user.pp_url; 
+          document.getElementById("avatar2").src = user.pp_url;
+          document.getElementById("full_name").innerHTML = user.full_name;
+          document.getElementById("fname2").innerHTML = user.full_name;
+          document.getElementById("phone_number").innerHTML = user.phone_number;
+          document.getElementById("email_address").innerHTML = user.email_address;
+          document.getElementById("user_type").innerHTML = user.user_type;
       }
     }
     hideLoading();
